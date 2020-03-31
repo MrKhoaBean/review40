08d0d13db108450aacb8380ff79a362a261f0044
Đậu Văn Đăng Khoa <46867018+MrKhoaBean@users.noreply.github.com>
2020-03-31 23:00:55 +0700

    vietsub và làm nó tốt hơn

--- index.js
+++ index.js
@@ -3,46 +3,151 @@ const app = express();
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const path = require('path');
 const PORT = 3000;
+
+const mysql = require('mysql');
+
+const connect = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+connect.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 app.set('view engine', 'ejs')
 app.use(bodyParser());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'views/admin')));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'somesecret',
+    cookie: { maxAge: 60000 }
+}));
 
-app.listen(PORT, ()=>{
-	console.log('port: *' + PORT);
+app.listen(PORT, () => {
+    console.log('port: *' + PORT);
 })
 
-app.get('/', (req, res)=>{
-	res.render('admin/index.ejs');
+
+app.use('/', (req, res, next) => {
+    if (!req.session.name)
+        req.session.name = 'khoa dep trai';
+    next();
+})
+
+app.get('/', (req, res) => {
+    //query và xử lí membersAddByMonth
+    let quyen = 'admin';
+    res.render(`${quyen}/index.ejs`, {
+        href: '/',
+        name: req.session.name,
+        memberCount: 100,
+        linkvideoCount: 10,
+        linkcmtCount: 5,
+        membersAddByMonth: [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0]
+    });
 })
 
 app.get('/quanliuser', (req, res) => {
-	res.render('admin/quanliuser.ejs');
+    //query và xử lí list user
+    let quyen = 'admin';
+    res.render(`${quyen}/quanliuser.ejs`, {
+        href: 'quanliuser',
+        name: req.session.name,
+        listUser: [{
+                name: 'Khoa ko mlem',
+                job: 'aaaa'
+            },
+            {
+                name: 'binh le',
+                job: 'bbbb'
+            },
+            {
+                name: 'hung',
+                job: 'ccccc'
+            },
+            {
+                name: 'vu hoang anh',
+                job: 'ddddd'
+            },
+            {
+                name: 'le tuan anh',
+                job: 'eeeee'
+            },
+            {
+                name: 'no name',
+                job: 'idk'
+            }
+        ]
+    });
+})
+
+app.post('/quanliuser', (req, res)=>{
+	//
 })
 
 app.get('/quanliadmin', (req, res) => {
-	res.render('admin/quanliadmin.ejs');
+    //query và xử lí 
+    let quyen = 'admin';
+    res.render(`${quyen}/quanliadmin.ejs`, {
+        href: 'quanliadmin',
+        name: req.session.name,
+        listAdmin: [{
+                name: 'alo',
+                job: 'aaaa'
+            },
+            {
+                name: '1',
+                job: 'sssssssssssss'
+            },
+            {
+                name: '2',
+                job: 'rrrrrrrrr'
+            },
+            {
+                name: '3',
+                job: '3333333'
+            },
+            {
+                name: '4',
+                job: 'asdasd'
+            },
+            {
+                name: 'donan jump',
+                job: 'president'
+            }
+        ]
+    });
 })
 
 app.get('/checkcmt', (req, res) => {
-	res.render('admin/checkcmt.ejs');
+    let quyen = 'admin';
+    res.render(`${quyen}/checkcmt.ejs`, { href: 'checkcmt', name: req.session.name });
 })
 
 app.get('/listlink', (req, res) => {
-	res.render('admin/listlink.ejs');
+    let quyen = 'admin';
+    res.render(`${quyen}/listlink.ejs`, { href: 'listlink', name: req.session.name });
 })
 
 app.get('/profile', (req, res) => {
-	res.render('admin/profile.ejs');
+    let quyen = 'admin';
+    res.render(`${quyen}/profile.ejs`, { href: 'profile', name: req.session.name });
 })
 
 app.get('/linkvideo', (req, res) => {
-	res.render('admin/linkvideo.ejs');
+    let quyen = 'admin';
+    res.render(`${quyen}/linkvideo.ejs`, { href: 'linkvideo', name: req.session.name });
 })
 
 app.get('/linkcmt', (req, res) => {
-	res.render('admin/linkcmt.ejs');
+    let quyen = 'admin';
+    res.render(`${quyen}/linkcmt.ejs`, { href: 'linkcmt', name: req.session.name });
 })
--- views/admin/js/chart.js
+++ views/admin/js/chart.js
@@ -1,3 +1,28 @@
+// $(function () {
+//     "use strict";
+
+//     var myChart = Highcharts.chart('chart', {
+//         chart: {
+//             type: 'areaspline'
+//         },
+//         title: {
+//             text: 'THỐNG KÊ'
+//         },
+//         xAxis: {
+//             categories: ['1', '2', '3','4', '5', '6']
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Số thành viên'
+//             }
+//         },
+//         series: [{
+//             name: 'Số thành viên tạo tài khoản theo tháng',
+//             data: [10,4,3,4,17,10,30,44,33,50,90,5,13]
+//         }]
+//     });
+// });
+
 $(function () {
     "use strict";
 
@@ -6,131 +31,20 @@ $(function () {
             type: 'areaspline'
         },
         title: {
-            text: ''
+            text: 'THỐNG KÊ'
         },
         xAxis: {
-            // categories: ['1', '2', '3','4', '5', '6']
+            categories: ['1', '2', '3','4', '5', '6', '7', '8', '9', '10', '11', '12']
         },
         yAxis: {
             title: {
-                text: ''
+                text: 'Số thành viên'
             }
         },
         series: [{
-            name: 'Số thành viên theo tháng',
-            data: [0,4,3,4,17,10,30,44,33,50,90,5,13]
+            name: 'Số thành viên tạo tài khoản theo tháng',
+            data: membersAddByMonth
         }]
     });
-
-
-    // Build the chart
-    Highcharts.chart('IndexChartOne', {
-      chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false,
-          type: 'pie'
-      },
-      title: {
-          text: 'Browser market'
-      },
-      tooltip: {
-          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-      },
-      plotOptions: {
-          pie: {
-              allowPointSelect: true,
-              cursor: 'pointer',
-              dataLabels: {
-                  enabled: false
-              },
-              showInLegend: true
-          }
-      },
-      series: [{
-          name: 'Brands',
-          colorByPoint: true,
-          data: [{
-              name: 'Chrome',
-              y: 22.41,
-              sliced: true,
-              selected: true
-          }, {
-              name: 'IE',
-              y: 11.84
-          }, {
-              name: 'FX',
-              y: 10.85
-          }, {
-              name: 'Edge',
-              y: 4.67
-          }]
-      }]
-    });
-
-    // Build the chart
-    Highcharts.chart('IndexChartTwo', {
-      chart: {
-        type: 'column'
-        },
-        title: {
-            text: 'Monthly Average Revenue'
-        },
-        subtitle: {
-            text: ''
-        },
-        xAxis: {
-            categories: [
-                'Jan',
-                'Feb',
-                'Mar',
-                'Apr',
-                'May',
-                'Jun',
-                'Jul',
-                'Aug',
-                'Sep',
-                'Oct',
-                'Nov',
-                'Dec'
-            ],
-            crosshair: true
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: ''
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Tokyo',
-            data: [49.9, 71.5, 106.4, 129.2]
-
-        }, {
-            name: 'London',
-            data: [48.9, 38.8, 39.3, 41.4]
-
-        }, {
-            name: 'Berlin',
-            data: [42.4, 33.2, 34.5, 39.7]
-
-        }]
-    });
-
-
-
 });
+
